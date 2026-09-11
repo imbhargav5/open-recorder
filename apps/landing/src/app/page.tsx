6f1eb393b2e8f7de98cbd884b001d524fdc48f85
Ponnapalli Bhargav <bhargavponnapalli.5@gmail.com>
2026-09-11 19:51:23 +0530

    refactor(landing): reuse page docs URL in nav

--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home(): ReactElement {
       <ScrollProgress />
       <MotionObserver />
 
-      <RedesignNav />
+      <RedesignNav docsUrl={docsUrl} />
       <RedesignHero docsUrl={docsUrl} sourceUrl={sourceUrl} />
       <RedesignProof />
       <RedesignFeatures />
